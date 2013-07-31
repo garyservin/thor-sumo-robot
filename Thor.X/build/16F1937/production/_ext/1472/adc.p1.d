4bbc89713e8868fe262644a21f7a36b9bde7be82
Gary Servin <garyservin@gmail.com>
2013-07-30 23:48:50 -0400

    Added gitignore file

--- Thor.X/build/16F1937/production/_ext/1472/adc.p1.d
+++
@@ -1,5 +0,0 @@
- build/16F1937/production/_ext/1472/adc.d  \
- build/16F1937/production/_ext/1472/adc.p1:  \
- ../adc.c  \
-../GenericTypeDefs.h  \
-../adc.h 
--- Thor.X/build/16F1937/production/_ext/1472/main.p1.d
+++
@@ -1,7 +0,0 @@
- build/16F1937/production/_ext/1472/main.d  \
- build/16F1937/production/_ext/1472/main.p1:  \
- ../main.c  \
-../thor.h  \
-../usart.h  \
-../GenericTypeDefs.h  \
-../adc.h 
